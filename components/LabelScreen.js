9396c72524d022729f4c1741dbe8f40b71f04db7
mirhabibul.latif@techhack.ca <mirsahib24@gmail.com>
2021-09-30 18:14:47 +0600

    :hammer: upload image to cloudinary

--- components/LabelScreen.js
+++ components/LabelScreen.js
@@ -1,25 +1,50 @@
-import { View, Text, TextInput, StyleSheet,Button } from "react-native";
+import { View, Text, TextInput, StyleSheet, Button } from "react-native";
 import React, { useState, useEffect } from 'react';
 import * as MediaLibrary from 'expo-media-library';
+import { Dropdown } from 'react-native-material-dropdown-v2-fixed';
+import { SECRET } from "../config";
+
 
 export default function LabelScreen({ route, navigation }) {
     const { uri } = route.params
-    const [data,setData] = useState({className:"",productName:"",MeasuringUnit:""})
-    const savePicture = async () => {
-        const status = await MediaLibrary.requestPermissionsAsync()
-        if (status.granted) {
-            const assert = await MediaLibrary.createAssetAsync(imageUri)
-            const result = await MediaLibrary.createAlbumAsync('Grocery', assert, true)
-            setImageUri(null)
-            console.log("image ", result)
-        }
-    }
-    const handleOnChange = (name,value)=>{
-        setData({...data,[name]:value})
+    const [data, setData] = useState({ className: "", productName: "", MeasuringUnit: "" })
+    const measuringUnit = [{
+        value: 'kg/g',
+    }, {
+        value: 'l/ml',
+    }, {
+        value: 'pcs',
+    }];
+
+    
+    const handleOnChange = (name, value) => {
+        setData({ ...data, [name]: value })
     }
 
-    const handleSubmit = ()=>{
-        console.log(data)
+    const handleSubmit = async () => {
+        let apiUrl = `https://api.cloudinary.com/v1_1/${SECRET}/image/upload`
+        let base64Img = `data:image/jpg;base64,${uri}`
+        let imageData = { file: base64Img, upload_preset: "annotationImage" }
+        //console.log("image data", imageData)
+        try{
+            let result = await fetch(apiUrl,{
+                method:"POST",
+                body:JSON.stringify(imageData),
+                headers:{'content-type': 'application/json'}
+            })
+            //console.log("result",result)
+            let response = await result.json()
+            console.log(response)
+        }catch(err){
+            console.log("error",err)
+        }
+
+
+        // save image using cloudlinarly api
+        // check image saved successfully
+        //save data with image public id using mongodb realm webhook
+        //false show error
+
     }
 
     return (
@@ -27,17 +52,19 @@ export default function LabelScreen({ route, navigation }) {
             <TextInput
                 style={styles.input}
                 placeholder={"Enter class name"}
-                onChangeText={(e)=>handleOnChange("className",e)}
+                onChangeText={(e) => handleOnChange("className", e)}
             />
             <TextInput
                 style={styles.input}
-                placeholder={"Enter Product Name"}
-                onChangeText={(e)=>handleOnChange("productName",e)}
+                placeholder={"Enter product Name"}
+                onChangeText={(e) => handleOnChange("productName", e)}
             />
-            <TextInput
-                style={styles.input}
-                placeholder={"Enter Measuring Unit"}
-                onChangeText={(e)=>handleOnChange("MeasuringUnit",e)}
+            <Dropdown
+                icon='chevron-down'
+                iconColor='#E1E1E1'
+                label='Measuring Unit'
+                onChangeText={e => handleOnChange("MeasuringUnit", e)}
+                data={measuringUnit}
             />
             <Button
                 title="Save"
